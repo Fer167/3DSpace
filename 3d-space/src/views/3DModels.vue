9986284f92a2a9a94cacbf205252d00093a1402a
Sergey <ferrumpao@gmail.com>
2023-05-02 12:02:23 +0300

    add: 3D models page

--- 3d-space/src/views/3DModels.vue
+++ 3d-space/src/views/3DModels.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="all-models">
+    <div class="all-models__title">
+      3D Модели всех пользователей
+    </div>
+    <MyWorks></MyWorks> 
   </div>
 </template>
+
+<script>
+import MyWorks from '@/components/MyWorks.vue';
+
+export default {
+  props: {
+  },
+  components: {
+    MyWorks,
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style scoped>
+.all-models {
+  width: 1280px;
+  margin: auto;
+}
+.all-models__title {
+  margin-top: 50px;
+  margin-bottom: 10px;
+  font-size: 20px;
+}
+</style>
